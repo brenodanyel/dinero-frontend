c65638aab9dcbe5cc6274cfaf62c868a2a8369c7
bdls <bdls1704@hotmail.com>
2025-01-12 16:19:49 -0300

    chore(deps): exclude problematic dependencies
    Temporarily excludes `@tailwindcss/oxide`, `globby`, and `lightningcss` from optimization during the dependency resolution process.
    
    This workaround addresses conflicts arising from the current versions of Tailwind CSS v4 and Nuxt UI, and should be reverted once official compatible releases are available (estimated January 2025).

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
-import ui from '@nuxt/ui/vite';
+import ui from '@nuxt/ui/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), ui()],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
-		},
-	},
-});
+  plugins: [vue(), ui()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  // temp (jan/2025): try to remove this in the future when tailwindv4 and nuxtui is released officially
+  optimizeDeps: {
+    exclude: ['@tailwindcss/oxide', 'globby', 'lightningcss'],
+  },
+})
