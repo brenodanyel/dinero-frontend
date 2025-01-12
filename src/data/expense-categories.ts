de384f4b63524609c692a7d3a442159fd952b242
bdls <bdls1704@hotmail.com>
2025-01-12 20:41:38 -0300

    feat(data): Add expense categories
    Introduces a new set of expense categories to be used throughout the application.  These categories include common expense types like food, education, leisure, housing, health, transportation, and a general "other" category.  Each category is associated with a specific icon for visual representation.

---
+++ src/data/expense-categories.ts
@@ -0,0 +1,42 @@
+export const expenseCategoriesArray = [
+  'Alimentação',
+  'Educação',
+  'Lazer',
+  'Moradia',
+  'Saúde',
+  'Transporte',
+  'Outros',
+] as const
+
+export type ExpenseCategory = (typeof expenseCategoriesArray)[number]
+
+type ExpenseCategories = Record<
+  ExpenseCategory,
+  {
+    icon: string
+  }
+>
+
+export const expenseCategoriesSettings: ExpenseCategories = {
+  Alimentação: {
+    icon: 'i-mdi-food-outline',
+  },
+  Educação: {
+    icon: 'i-mdi-education-outline',
+  },
+  Lazer: {
+    icon: 'i-mdi-emoticon-happy-outline',
+  },
+  Moradia: {
+    icon: 'i-mdi-house-outline',
+  },
+  Saúde: {
+    icon: 'i-mdi-heart-outline',
+  },
+  Transporte: {
+    icon: 'i-mdi-car-outline',
+  },
+  Outros: {
+    icon: 'i-mdi-dots-horizontal',
+  },
+}
