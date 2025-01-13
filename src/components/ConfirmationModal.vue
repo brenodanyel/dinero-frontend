cb5c09d3ee9e5ca20d74432725803b58e7490244
bdls <bdls1704@hotmail.com>
2025-01-12 21:43:12 -0300

    feat(modal): Adds confirmation modal component
    This commit introduces a reusable confirmation modal component.
    
    The component displays a title, description, and two buttons for confirming or canceling an action. It emits events for both 'confirm' and 'cancel' actions, allowing parent components to handle the respective logic.

---
+++ src/components/ConfirmationModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <UModal>
+    <template #title>{{ title }} </template>
+    <template #description> {{ description }}</template>
+
+    <template #footer>
+      <UButton
+        class="w-40"
+        block
+        label="Cancelar"
+        variant="outline"
+        color="error"
+        @click="$emit('cancel')"
+      />
+
+      <UButton class="w-40" block label="Salvar" loading-auto @click="$emit('confirm')" />
+    </template>
+  </UModal>
+</template>
+
+<script setup lang="ts">
+defineEmits(['confirm', 'cancel'])
+
+defineProps<{
+  title: string
+  description: string
+}>()
+</script>
